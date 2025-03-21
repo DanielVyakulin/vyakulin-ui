f27ac911bb827a34216048cc63f20025b69f5060
vyakulindaniel <nilvlin@mail.ru>
2025-03-21 18:07:27 +0400

    Добавлен пакет clsx для управления классами. Обновлены компоненты: добавлены новые компоненты Snip и BreadCrumb, изменены классы на использование clsx для улучшения управления стилями. Обновлены стили и переменные в SCSS. Исправлены некоторые параметры стилей для кнопок и отступов.

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,15 @@
 'use client';
+import { Snip } from './components/elements';
+import { SiGnubash } from "react-icons/si";
 
 export default function Home() {
   return (
     <main className="all">
+      <Snip name="bash" icon={<SiGnubash />}>
+        npm install vyakulin-ui <br />
+        <span># or</span> <br />
+        pnpm add vyakulin-ui
+      </Snip>
     </main>
   );
 } 
