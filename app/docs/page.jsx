cba4d6cf9603cee3cd2d0b997bc099854d2c4bb4
vyakulindaniel <nilvlin@mail.ru>
2025-03-21 23:00:39 +0400

    Упрощены компоненты в elements.jsx для улучшения читаемости и управления классами. Обновлены стили в globals.scss: добавлены миксины для управления flex, переходами и обводками, а также упрощены стили для кнопок и ссылок. Реализована генерация стилей для интерактивных элементов и инпутов.

---
+++ app/docs/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from '../components/elements';
+
+export default function Docs() {
+  return (
+    <React.Fragment>
+      <Text text="h1">Docs</Text>
+    </React.Fragment>
+  );
+}
